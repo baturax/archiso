daa014161acca43609d3110d8e7aef68fe1e95d6
Bai <bai@baturax.com>
2025-05-05 12:49:23 +0300

    .

--- airootfs/etc/skel/.config/fish/config.fish
+++ airootfs/etc/skel/.config/fish/config.fish
@@ -1,24 +1,49 @@
-#source /usr/share/cachyos-fish-config/cachyos-config.fish
+   #  Git
+alias gclone="git clone"
+alias gpush="git push"
+alias gpull="git pull"
+alias gcommit="git commit"
+
+   #  System
+alias pi="sudo pacman -S"
+alias pu="sudo pacman -Suy"
+alias pr="sudo pacman -R"
+alias prc="sudo pacman -Rnsc"
+alias prd="sudo pacman -Rdd"
+
+   #  Misc
+alias ls="eza --color=always --color-scale-mode=gradient --icons=always --hyperlink -a --group-directories-first -s=Extension"
+alias cat="bat"
+alias ch="chmod +x"
+alias mc="meson compile"
+alias man="batman"
+alias c="printf '\033[2J\033[3J\033[1;1H'"
+
+   #  Shortcut
+alias free="free -h"
+alias r="rm -rfiv"
+alias m="mkdir -pv"
+
+   #  Exports
+export gh="https://github.com/"
+
+   #  Functions
+
+   # Better exit
+function exit
+    jobs -q; and disown (jobs -p)
+    builtin exit
+end
+
+   # Fish_Greeting
 function fish_greeting
     fastfetch -c examples/8.jsonc
 end
 
-alias gpush="git add . && git commit -m 'bai' && git push"
-alias ch="chmod +x"
-alias sudo="sudo -E"
-alias ls="eza --color=always --color-scale-mode=gradient --icons=always --hyperlink -a --group-directories-first -s=Extension"
-alias cat="bat"
-alias c="clear"
-alias gc="git clone"
-alias pi="sudo pacman -S"
-alias pr="sudo pacman -R"
-alias prc="sudo pacman -Rnc"
-alias prd="sudo pacman -Rdd"
-alias free="free -h"
-alias r="rm -rf"
-alias m="mkdir -p"
-
-function exit
-    jobs -q; and disown (jobs -p)
-    builtin exit
+   #  Cd verbose
+function cd
+   set -l prev_pwd (pwd)
+   if builtin cd $argv
+   printf '%s -> %s\n' $prev_pwd (pwd)
+   end
 end
--- airootfs/etc/skel/.config/nvim/lua/config/settings.lua
+++ airootfs/etc/skel/.config/nvim/lua/config/settings.lua
@@ -1,13 +1,40 @@
-vim.g.mapleader = " "
+local opt = vim.opt
+local o = vim.o
+local g=vim.g
 
-vim.o.tabstop = 3
-vim.o.expandtab = true 
-vim.o.softtabstop = 3 
-vim.o.shiftwidth = 3
+g.mapleader = " "
 
-vim.opt.clipboard:append("unnamedplus")
-vim.opt.undofile = true
+o.tabstop = 3
+o.expandtab = true 
+o.softtabstop = 3 
+o.shiftwidth = 3
+
+opt.clipboard:append("unnamedplus")
+opt.undofile = true
 vim.wo.number = true
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
+loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+opt.termguicolors = true
+opt.shortmess:append "sI"
+opt.shortmess:append "sI"
+opt.scrolloff = 10
+
+vim.cmd("NoNeckPain")
+
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+local modes = { "n", "i" }
+
+for i = 1, 9 do
+  map(modes, "<A-" .. i .. ">", "<Cmd>BufferGoto " .. i .. "<CR>", opts)
+end
+
+map(modes, "<A-w>", "<Cmd>BufferClose<CR>", opts)
+
+map("i", "<A-e>", "<Esc>:Telescope file_browser path=%:p:h select_buffer=true<CR>", opts)
+map("n", "<A-e>", "<Cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>", opts)
+
+vim.keymap.set('n', '<A-Right>', '<C-w>l')
+vim.keymap.set('n', '<A-Left>', '<C-w>h')
+
